aa3e2ca9c75f4c15089a38f8673f9b7e54a57caf
Diego Zamora <diego.dgl10@ciencias.unam.mx>
2025-04-05 18:21:46 -0600

    Intento de modularizar codigo HTML

---
+++ js/cargaComponente.js
@@ -0,0 +1,32 @@
+let ruta = './'
+ruta = '/views/components/'
+
+function setRuta(nuevaRuta) {
+    ruta = nuevaRuta
+}
+
+/**
+ * Funcion que carga componentes de la vista
+ * @param {*} id el id del componente html donde se colocara el nuevo componente
+ * @param {*} componente la ruta del nuevo componente
+ */
+function cargaComponente(id, componente) {
+    $.get(`/views/components/${componente}.html`, function(data) {
+        $(`#${id}`).prepend(data);
+    });
+}
+
+/**
+ * Funcion que carga los componentes utilizados en toda la pagina
+ */
+function componentesGenerales() {
+    let componentes = [
+        {'id': 'navbar', 'componente':'navbar'},
+        {'id': 'footer', 'componente':'footer'},
+        {'id': 'head', 'componente':'head'},
+    ]
+
+    componentes.forEach(element => {
+        cargaComponente(element.id, element.componente)
+    })
+}
